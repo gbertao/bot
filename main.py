d05c2e42d6210bd8fe3448a6bf600d37f60e8558
joaopmt <joaop.congiomartins@gmail.com>
2019-03-11 11:52:29 -0300

    last commit

--- main.py
+++ main.py
@@ -1,84 +1,127 @@
-import http.server
-import json
-import asyncio
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
-from botbuilder.schema import (Activity, ActivityTypes)
-from botframework.connector import ConnectorClient
-from botframework.connector.auth import (MicrosoftAppCredentials,
-                                         JwtTokenValidation, SimpleCredentialProvider)
+"""
+This sample shows how to use different types of rich cards.
+"""
+
+
+from aiohttp import web
+from botbuilder.schema import (Activity, ActivityTypes,
+                               AnimationCard, AudioCard, Attachment,
+                               ActionTypes, CardAction,
+                               CardImage, HeroCard,
+                               MediaUrl, ThumbnailUrl,
+                               ThumbnailCard, VideoCard,
+                               ReceiptCard, SigninCard,
+                               Fact, ReceiptItem)
+from botbuilder.core import (BotFrameworkAdapter, BotFrameworkAdapterSettings, TurnContext,
+                             ConversationState, MemoryStorage, UserState, CardFactory)
+"""Import AdaptiveCard content from adjacent file"""
+from adaptive_card_example import ADAPTIVE_CARD_CONTENT
 
 APP_ID = ''
 APP_PASSWORD = ''
+PORT = 9000
+SETTINGS = BotFrameworkAdapterSettings(APP_ID, APP_PASSWORD)
+ADAPTER = BotFrameworkAdapter(SETTINGS)
+
+# Create MemoryStorage, UserState and ConversationState
+memory = MemoryStorage()
+# Commented out user_state because it's not being used.
+# user_state = UserState(memory)
+conversation_state = ConversationState(memory)
+
+# Register both State middleware on the adapter.
+# Commented out user_state because it's not being used.
+# ADAPTER.use(user_state)
+ADAPTER.use(conversation_state)
+
+def create_thumbnail_card() -> Attachment:
+    card = ThumbnailCard(title="BotFramework Thumbnail Card", subtitle="Your bots — wherever your users are talking",
+                         text="Build and connect intelligent bots to interact with your users naturally wherever"
+                              " they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.",
+                         images=[],
+                         buttons=[CardAction(type=ActionTypes.open_url, title="Get Started",
+                                             value="https://docs.microsoft.com/en-us/azure/bot-service/")])
+    return CardFactory.thumbnail_card(card)
 
 
-class BotRequestHandler(http.server.BaseHTTPRequestHandler):
+async def create_reply_activity(request_activity: Activity, text: str, attachment: Attachment = None) -> Activity:
+    activity = Activity(
+        type=ActivityTypes.message,
+        channel_id=request_activity.channel_id,
+        conversation=request_activity.conversation,
+        recipient=request_activity.from_property,
+        from_property=request_activity.recipient,
+        text=text,
+        service_url=request_activity.service_url)
+    if attachment:
+        activity.attachments = [attachment]
+    return activity
 
-    @staticmethod
-    def __create_reply_activity(request_activity, text):
-        return Activity(
-            type=ActivityTypes.message,
-            channel_id=request_activity.channel_id,
-            conversation=request_activity.conversation,
-            recipient=request_activity.from_property,
-            from_property=request_activity.recipient,
-            text=text,
-            service_url=request_activity.service_url)
 
-    def __handle_conversation_update_activity(self, activity):
-        self.send_response(202)
-        self.end_headers()
-        if activity.members_added[0].id != activity.recipient.id:
-            credentials = MicrosoftAppCredentials(APP_ID, APP_PASSWORD)
-            reply = BotRequestHandler.__create_reply_activity(activity, 'Hello and welcome to the echo bot!')
-            connector = ConnectorClient(credentials, base_url=reply.service_url)
-            connector.conversations.send_to_conversation(reply.conversation.id, reply)
-
-    def __handle_message_activity(self, activity):
-        self.send_response(200)
-        self.end_headers()
-        credentials = MicrosoftAppCredentials(APP_ID, APP_PASSWORD)
-        connector = ConnectorClient(credentials, base_url=activity.service_url)
-        reply = BotRequestHandler.__create_reply_activity(activity, 'You said: %s' % activity.text)
-        connector.conversations.send_to_conversation(reply.conversation.id, reply)
-
-    def __handle_authentication(self, activity):
-        credential_provider = SimpleCredentialProvider(APP_ID, APP_PASSWORD)
-        loop = asyncio.new_event_loop()
-        try:
-            loop.run_until_complete(JwtTokenValidation.authenticate_request(
-                activity, self.headers.get("Authorization"), credential_provider))
-            return True
-        except Exception as ex:
-            self.send_response(401, ex)
-            self.end_headers()
-            return False
-        finally:
-            loop.close()
-
-    def __unhandled_activity(self):
-        self.send_response(404)
-        self.end_headers()
-
-    def do_POST(self):
-        body = self.rfile.read(int(self.headers['Content-Length']))
-        data = json.loads(str(body, 'utf-8'))
-        activity = Activity.deserialize(data)
-
-        if not self.__handle_authentication(activity):
-            return
-
-        if activity.type == ActivityTypes.conversation_update.value:
-            self.__handle_conversation_update_activity(activity)
-        elif activity.type == ActivityTypes.message.value:
-            self.__handle_message_activity(activity)
+async def handle_message(context: TurnContext) -> web.Response:
+    # Access the state for the conversation between the user and the bot.
+    state = await conversation_state.get(context)
+    if hasattr(state, 'in_prompt'):
+        if state.in_prompt:
+            state.in_prompt = False
+            return await card_response(context)
         else:
-            self.__unhandled_activity()
+            state.in_prompt = True
+            prompt_message = await create_reply_activity(context.activity, 'Wich subject you would like to look up? (ex: mc102)')
+            await context.send_activity(prompt_message)
+            return web.Response(status=202)
+    else:
+        state.in_prompt = True
+        prompt_message = await create_reply_activity(context.activity, 'Wich subject you would like to look up? (ex: mc102)')
+        await context.send_activity(prompt_message)
+        return web.Response(status=202)
 
+
+async def card_response(context: TurnContext) -> web.Response:
+    response = context.activity.text.strip()
+    card = create_thumbnail_card()
+    response = await create_reply_activity(context.activity, '', card)
+    await context.send_activity(response)
+    return web.Response(status=200)
+
+
+async def handle_conversation_update(context: TurnContext) -> web.Response:
+    if context.activity.members_added[0].id != context.activity.recipient.id:
+        response = await create_reply_activity(context.activity, 'Welcome to the subject-finder Bot!')
+        await context.send_activity(response)
+    return web.Response(status=200)
+
+
+async def unhandled_activity() -> web.Response:
+    return web.Response(status=404)
+
+
+async def request_handler(context: TurnContext) -> web.Response:
+    if context.activity.type == 'message':
+        return await handle_message(context)
+    elif context.activity.type == 'conversationUpdate':
+        return await handle_conversation_update(context)
+    else:
+        return await unhandled_activity()
+
+
+async def messages(req: web.Request) -> web.Response:
+    body = await req.json()
+    activity = Activity().deserialize(body)
+    auth_header = req.headers['Authorization'] if 'Authorization' in req.headers else ''
+    try:
+        return await ADAPTER.process_activity(activity, auth_header, request_handler)
+    except Exception as e:
+        raise e
+
+
+app = web.Application()
+app.router.add_post('/', messages)
 
 try:
-    SERVER = http.server.HTTPServer(('localhost', 9000), BotRequestHandler)
-    print('Started http server on localhost:9000')
-    SERVER.serve_forever()
-except KeyboardInterrupt:
-    print('^C received, shutting down server')
-    SERVER.socket.close()
+    web.run_app(app, host='localhost', port=PORT)
+except Exception as e:
+    raise e
